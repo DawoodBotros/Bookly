8da2723fbb069b98c8ea93cdfbb5139bdf411d4c
Pop <dododawood236@gmail.com>
2023-10-28 23:23:10 +0200

    finish My App

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/my_observer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'Features/home/data/repos/home_repo_impl.dart';
@@ -10,6 +11,7 @@ import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   setupServiceLocator();
+  Bloc.observer =MyBlocObserver();
   runApp(const Bookly());
 }
 
