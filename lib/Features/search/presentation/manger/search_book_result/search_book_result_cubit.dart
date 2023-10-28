0bd62cf6f3fcc037b5a0420f1bf5993f221b33d3
Pop <dododawood236@gmail.com>
2023-10-28 19:42:20 +0200

    finish My App

--- lib/Features/search/presentation/manger/search_book_result/search_book_result_cubit.dart
+++ lib/Features/search/presentation/manger/search_book_result/search_book_result_cubit.dart
@@ -1,15 +1,15 @@
 import 'package:bookly_app/Features/search/presentation/manger/search_book_result/seatch_book_result_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
-import '../../../../home/data/repos/home_repo.dart';
 import '../../../repos/home_repo.dart';
 
 class SearchBooksCubit extends Cubit<SearchBooksState> {
   SearchBooksCubit(this.searchRepo) : super(SearchBooksInitial());
 
   final SearchRepo searchRepo;
+
   static SearchBooksCubit get(BuildContext context) => BlocProvider.of(context);
+
   Future<void> fetchSearchBooks({required String bookName}) async {
     emit(SearchBooksLoading());
     var result = await searchRepo.fetchSearchBooks(bookName: bookName);
@@ -19,5 +19,6 @@ class SearchBooksCubit extends Cubit<SearchBooksState> {
       emit(SearchBooksSuccess(books));
     });
   }
+
   TextEditingController searchController = TextEditingController();
 }
